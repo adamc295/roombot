f1a814481ba07da0c5c7384209d8c522d9a73170
adamc295 <18663194+adamc295@users.noreply.github.com>
2019-04-06 17:22:48 +0100

    rewrite #2: electric boogalo
    i swear it the last one

---
+++ bot/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"flag"
+	"github.com/adamc295/roombot/common"
+)
+
+var (
+	f_token string
+)
+
+func init() {
+	flag.StringVar(&f_token, "token", "", "The token for the bot")
+}
+
+func main() {
+	flag.Parse()
+	
+	log.SetFormatter(&log.TextFormatter {
+		DisableTimestamp: true,
+		ForceColors: false,
+	})
+	
+	// No token? Get out!
+	if f_token == "" {
+		log.Error("No token specified. Use -token to set the bot token")
+		return
+	}
+	
+	log.Info("Starting RoomBot...")
+}
